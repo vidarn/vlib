02837f2b18f0f294b563dacde4d666f23ee7d6c0
Vidar Nelson <vidar.nelson@gmail.com>
2019-01-21 11:06:25 +0100

    some stuff

---
+++ src/libs/common_graphics/common_graphics.h
@@ -0,0 +1,7 @@
+#pragma once
+float smootherstep(float edge0, float edge1, float x)
+;
+float smoothstep(float edge0, float edge1, float x)
+;
+float clamp(float x, float lowerlimit, float upperlimit)
+;
---
+++ src/libs/gui/vintr_gui.h
@@ -0,0 +1,7 @@
+#pragma once
+
+int vintr_load_gui_font(struct GameData *data)
+;
+void vintr_set_gui_style(struct nk_context *ctx)
+;
+
