2543725dd2366658d8123157e3551699ee0c3cb6
Vidar Nelson <vidar.nelson@gmail.com>
2019-11-13 16:44:07 +0100

    Added function for getting the size of a memory mapped ffile

--- src/libs/mmap/mmap.h
+++ src/libs/mmap/mmap.h
@@ -9,5 +9,8 @@ struct MMapContext *mmap_file_read(const char *filename)
 void mmap_close(struct MMapContext *context)
 ;
 
+size_t mmap_size(struct MMapContext *context)
+;
+
 void *mmap_get_ptr(struct MMapContext *context)
 ;
--- src/libs/mmap/mmap_win32.c
+++ src/libs/mmap/mmap_win32.c
@@ -5,16 +5,23 @@ struct MMapContext
 {
 	HANDLE file_handle;
 	HANDLE mmap_handle;
+	size_t len;
 } ;
 
 struct MMapContext *mmap_file_read(const char *filename)
 {
 	HANDLE file_handle = CreateFileA(filename, GENERIC_READ, FILE_SHARE_READ,
 		NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+	if (file_handle == INVALID_HANDLE_VALUE) {
+		return 0;
+	}
+	LARGE_INTEGER file_size = { 0 };
+	GetFileSizeEx(file_handle, &file_size);
 	HANDLE mmap_handle = CreateFileMappingA(file_handle, NULL, PAGE_READONLY, 0, 0, NULL);
 	struct MMapContext *ret = HeapAlloc(GetProcessHeap(), HEAP_GENERATE_EXCEPTIONS, sizeof(struct MMapContext));
 	ret->file_handle = file_handle;
 	ret->mmap_handle = mmap_handle;
+	ret->len = file_size.QuadPart;
 	return ret;
 }
 
@@ -23,6 +30,11 @@ void mmap_close(struct MMapContext *context)
 	CloseHandle(context->mmap_handle);
 	CloseHandle(context->file_handle);
 	HeapFree(GetProcessHeap(), 0, context);
+}
+
+size_t mmap_size(struct MMapContext* context)
+{
+	return context->len;
 }
 
 void *mmap_get_ptr(struct MMapContext *context)
